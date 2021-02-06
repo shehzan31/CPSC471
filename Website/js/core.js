ab8ef3d0e4f26286564775a88aeab102c201497b
Tyler Hartleb <tyler.hartleb@gmail.com>
2021-02-05 20:19:39 -0700

    Added comments.

--- Website/js/core.js
+++ Website/js/core.js
@@ -1,7 +1,18 @@
+/*
+    Authors:            Tyler Hartleb,
+    Initial Date:       02/05/2021
+    Last Update:        02/05/2021
+    Version:            0.1
+
+    This provides core javascript functions.
+*/
+
+/* The purpose of this function is to toggle a class onto a html id */
 function toggleClassActive(id) {
     id.classList.toggle("active");
 }
 
+/* The purpose of this is to determine when the transition for the mobile menu has ended */
 function menuAnimation(id, sid) {
     if (id.classList.contains("active")) {
         id.classList.add("menu_closing");
@@ -18,6 +29,7 @@ function menuAnimation(id, sid) {
 
 }
 
+/* This combines the previous two functions to create a smooth scrolling animation for the mobile menu */
 function smoothScroll(id, sid) {
     menuAnimation(id, sid);
     toggleClassActive(id);
