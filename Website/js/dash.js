16e0129d7c91c8fdb6b069a6144d0f84146b1998
tylerHartleb <tyler@hartleb.ca>
2021-02-17 11:59:57 -0700

    More changes to the react components
    Added sample containers, these will be changed when we have the data from our database to input. There is also a sample function for outputting a name and profile image.

--- Website/js/dash.js
+++ Website/js/dash.js
@@ -4,9 +4,41 @@ const e = React.createElement;
 
 class Dash extends React.Component {
     render() {
-        return e("h1",null, "Dashboard")
+      return e("div", null, e("h1", { className: "dashboard_text"}, " Dashboard "), 
+      e("SampleDash", {className: "dash_info_pane"}));
     }
-}
+  
+  }
+  
+function SampleDash() {
+    return e("div", null);
+  }
 
 const domContainer = document.querySelector('#dashboard');
 ReactDOM.render(e(Dash), domContainer);
+
+
+
+class Appoint extends React.Component {
+    render() {
+        return e("h1", null, "Appointments")
+    }
+}
+
+const domContainer1 = document.querySelector('#appointments');
+ReactDOM.render(e(Appoint), domContainer1);
+
+const name_s = "John Doe";
+
+class Head extends React.Component {
+    render() {
+      return React.createElement("div", null, React.createElement("h1", {
+        className: "header_text"
+      }, "", name_s), React.createElement("div", {
+        className: "profile_image"
+      }));
+    } 
+  }
+
+const domContainer2 = document.querySelector('#header');
+ReactDOM.render(e(Head), domContainer2);
