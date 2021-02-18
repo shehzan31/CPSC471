018621264af597247fe8f4001968684ae8d66889
tylerHartleb <tyler@hartleb.ca>
2021-02-17 18:21:14 -0700

    Some cleaning up
    looks better now, structural test code is good.

--- Website/js/dash.js
+++ Website/js/dash.js
@@ -21,8 +21,9 @@ ReactDOM.render(e(Dash), domContainer);
 
 class Appoint extends React.Component {
     render() {
-        return e("h1", null, "Appointments")
-    }
+        return e("div", null, e("h1", { className: "appoint_text"}, " Appointments "), 
+        e("SampleDash", {className: "appoint_info_pane"}));
+      }
 }
 
 const domContainer1 = document.querySelector('#appointments');
@@ -30,13 +31,24 @@ ReactDOM.render(e(Appoint), domContainer1);
 
 
 class Test extends React.Component {
-  render() {
-      return e("h1", null, "Patient Tests")
-  }
+    render() {
+        return e("div", null, e("h1", { className: "tests_text"}, " Tests "), 
+        e("SampleDash", {className: "tests_info_pane"}));
+      }
 }
 
-const domContainer3 = document.querySelector('#test');
+const domContainer3 = document.querySelector('#tests');
 ReactDOM.render(e(Test), domContainer3);
+
+class Pres extends React.Component {
+    render() {
+        return e("div", null, e("h1", { className: "pres_text"}, " Prescriptions "), 
+        e("SampleDash", {className: "pres_info_pane"}));
+      }
+}
+
+const domContainer4 = document.querySelector('#prescriptions');
+ReactDOM.render(e(Pres), domContainer4);
 
 const name_s = "John Doe";
 
