ce1ae12d35acc61059f0df32da768e007424afdd
nicholas-mclaughlin <nickryu4718@gmail.com>
2021-04-05 16:09:53 -0600

    Create admin.php

---
+++ Database/api/objects/admin.php
@@ -0,0 +1,63 @@
+<?php
+class Admin {
+    // database connection and table name
+    private $conn;
+    private $table_name = "Admin";
+    private $database = "HealthDatabase";
+
+    // Object properties
+    public $Admin_ID;
+    public $Master_Login;
+
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    // function read() {
+    //     $query =   "SELECT 
+    //                 *
+    //                 FROM
+    //                     ". $this->database . "." . $this->table_name ."";
+        
+    //     //prepare query statement
+    //     $stmt = $this->conn->prepare($query); 
+
+    //     //execute query 
+    //     $stmt->execute();
+    //     return $stmt;
+    // }
+
+    // function sin($sin) {
+    //     $query =   "SELECT *
+    //                 FROM $this->database.$this->table_name as p
+    //                 WHERE p.SIN = $sin";
+
+    //     $stmt = $this->conn->prepare($query);
+
+    //     $stmt->execute();
+    //     return $stmt;
+    // }
+
+    function new_doctor($Doctor_ID, $sin) {
+        $query =   "INSERT INTO $this->database.Doctor(Doctor_ID, SIN)
+                    VALUES ($Doctor_ID, $sin);
+
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->execute();
+        return $stmt;
+    }
+
+    function doctor_info($Doctor_ID) {
+        $query =   "SELECT *
+                    FROM $this->database.Doctor as d
+                    WHERE d.Doctor_ID = $Doctor_ID";
+
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->execute();
+        return $stmt;
+    }
+}
+?>
