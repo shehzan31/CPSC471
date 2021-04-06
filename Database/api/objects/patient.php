97cd0ffe155f0e0393d2898d9449b1bfac9f6f46
shehzan31 <47336141+shehzan31@users.noreply.github.com>
2021-04-05 19:24:32 -0500

    Added patient methods

---
+++ Database/api/objects/patient.php
@@ -0,0 +1,43 @@
+<?php
+class Patient{
+    // database connection and table name
+    private $conn;
+    private $table_name = "Patient";
+    private $database = "HealthDatabase";
+
+    // Object Properties
+    public $H_Number;
+    public $MR_Number;
+    public $SIN;
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    function read(){
+        $query =   "SELECT
+                    *
+                    FROM
+                        ".$this->database . "." . $this->table_name ."";
+        
+        //prepare query statement
+        $stmt = $this->conn->prepare($query);
+
+        //execute query
+        $stmt->execute();
+        return $stmt;
+    }
+
+    function mr_number($h_number) {
+        $query =   "SELECT *
+                    FROM $this->database.$this->table_name as p
+                    WHERE p.H_Number = $h_number";
+
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->execute();
+        return $stmt;
+    }
+}
+
+?>
