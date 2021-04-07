725ac8c6f87674787a0ce315e15f060c37aa2b5d
shehzan31 <47336141+shehzan31@users.noreply.github.com>
2021-04-06 20:51:04 -0500

    added another end point (MR_Appointments)

---
+++ Database/api/objects/medical_record_appointments.php
@@ -0,0 +1,30 @@
+<?php
+class Medical_Records_Appointments{
+    // database connection and table name
+    private $conn;
+    private $table_name = "MR_Appointments";
+    private $database = "HealthDatabase";
+
+    //Object properties
+    public $MR_Number;
+    public $Appointment;
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    function showAllApointments($mr_number){
+        $query =   "SELECT *
+                    FROM $this->database.$this->table_name as a
+                    WHERE a.MR_Number = $mr_number";
+        
+        //prepare query statement
+        $stmt = $this->conn->prepare($query);
+
+        //execute query
+        $stmt->execute();
+        return $stmt;
+    }
+
+}
+?>
