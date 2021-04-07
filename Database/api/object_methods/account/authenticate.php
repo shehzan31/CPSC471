65fac42eee473a9a3462fc26532366df418d6352
tylerHartleb <tyler@hartleb.ca>
2021-04-07 00:25:28 -0600

    Update authenticate.php

--- Database/api/object_methods/account/authenticate.php
+++ Database/api/object_methods/account/authenticate.php
@@ -2,8 +2,8 @@
     //session_start();
 
     // include database and object files
-    include_once '../../config/database.php';
-    include_once '../../objects/account/account.php';
+    include_once '../Database/api/config/database.php';
+    include_once '../Database/api/objects/account/account.php';
   
     // instantiate database and product object
 
