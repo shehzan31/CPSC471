0c626893fb7c787f84309e20b5f8d1b260580df5
adarshakanel <52978035+adarshakanel@users.noreply.github.com>
2021-04-16 14:04:01 -0600

    static posts

---
+++ Database/api/object_methods/finds/static_post.php
@@ -0,0 +1,27 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+// include database and object files
+include_once '../../config/database.php';
+include_once '../../objects/finds.php';
+
+// instantiate database and product object
+$database = new Database();
+$db = $database->getConnection();
+
+// initialize object
+$finds= new Finds($db);
+
+// Object Properties
+$Doctor_ID = 123456789;
+$H_Number = 200200200;
+$Condition = "headache";
+$Date = "2021-01-16";
+$Chart = "stress";
+
+// query products
+$stmt = $finds->post($Doctor_ID, $H_Number, $Condition, $Date, $Chart);
+
+?>
